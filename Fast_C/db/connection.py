07e590607b2fff87ef4883aa3cc90a22f4e01479
MenachemKazido <agbg7172@gmail.com>
2026-01-21 10:25:58 +0200

    connection to sql

--- Fast_C/db/connection.py
+++ Fast_C/db/connection.py
@@ -1,35 +1,41 @@
 import pymysql
 
-mydb = pymysql.connect(
-  host="localhost",
-  user="root",
-  password="menachem"
-)
+
+class Connection:
+    def __init__(self):
+        self.conn = None
+
+    def get_connection(self):
+        if self.conn is None:
+            self.conn = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="menachem")
+
+    def crate_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
+        cursor.execute("USE mydatabase")
+        self.conn.commit()
+        cursor.close()
+
+    def create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather_records
+        (id INT AUTO_INCREMENT PRIMARY KEY ,
+        timestamp DATETIME ,
+        location_name VARCHAR(50),
+        country VARCHAR(50) ,
+        latitude FLOAT ,
+        longitude FLOAT ,
+        temperature FLOAT ,
+        wind_speed  FLOAT ,
+        humidity INT ,
+        temperature_category VARCHAR(50),
+        wind_category VARCHAR(50))""")
+        self.conn.commit()
+        cursor.close()
 
 
 
-
-cursor = mydb.cursor()
-
-cursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
-cursor.execute("USE mydatabase")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS weather_records
-(id INT AUTO_INCREMENT PRIMARY KEY ,
-timestamp DATETIME ,
-location_name VARCHAR(50),
-country VARCHAR(50) ,
-latitude FLOAT ,
-longitude FLOAT ,
-temperature FLOAT ,
-wind_speed  FLOAT ,
-humidity INT ,
-temperature_category VARCHAR(50),
-wind_category VARCHAR(50))""")
-
-
-# cursor.execute("SHOW TABLES")
-#
-# for x in cursor:
-#   print(x)
